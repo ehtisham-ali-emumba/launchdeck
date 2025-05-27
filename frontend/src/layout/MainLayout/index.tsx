5dc26da2228ae99ab93f3277c6e73e29d7c561bc
ehtishamali042 <85542150+ehtisham-ali-emumba@users.noreply.github.com>
2025-05-27 12:07:09 +0500

    disable footer

--- frontend/src/layout/MainLayout/index.tsx
+++ frontend/src/layout/MainLayout/index.tsx
@@ -2,14 +2,14 @@ import React from "react";
 import { Header } from "./Header";
 import { RelativeDiv } from "./elements";
 import type { MainLayoutProps } from "./type";
-import { Footer } from "./Footer";
+// import { Footer } from "./Footer";
 
 export const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
   return (
     <RelativeDiv>
       <Header />
       <main>{children}</main>
-      <Footer />
+      {/* <Footer /> */}
     </RelativeDiv>
   );
 };
