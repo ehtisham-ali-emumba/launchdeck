5f48914afb807fde595b6499aef87c326548a11c
ehtishamali042 <85542150+ehtisham-ali-emumba@users.noreply.github.com>
2025-05-29 12:30:41 +0500

    refactor data

--- frontend/src/layout/MainLayout/Header/ProductsDropdown/types.ts
+++ frontend/src/layout/MainLayout/Header/ProductsDropdown/types.ts
@@ -1,14 +1,10 @@
 export interface SubCategory {
   id: string;
   name: string;
-  description: string;
-  slug: string;
-  icon: string;
 }
 
 export interface Category {
   id: string;
   name: string;
-  description: string;
   subCategories: SubCategory[];
 }
